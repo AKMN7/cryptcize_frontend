3f291c6a2cee14278c25ddd5941d97b18e545b0c
Anas864 <58662561+Anas864@users.noreply.github.com>
2022-12-16 17:18:53 +0300

    Updated Backend URL

--- src/store/auth/actions.js
+++ src/store/auth/actions.js
@@ -6,249 +6,239 @@ let timer;
 const period = 720000; // 12min
 
 export default {
+	// Sign In Functionality
+	async signIn(context, payload) {
+		// API EndPoint
+		const endPoint = "https://cooperative-deer-overalls.cyclic.app/api/v1/users/signin";
 
-    // Sign In Functionality
-    async signIn(context, payload) {
+		const data = {
+			email: payload.email,
+			password: payload.password,
+		};
 
-        // API EndPoint
-        const endPoint = "https://cryptcize.herokuapp.com/api/v1/users/signin";
+		// Send HTTP Request
+		const response = await axios.post(endPoint, data);
 
-        const data = {
-            email: payload.email,
-            password: payload.password
-        };
+		// If Unauthorized, Stop and Throw Error
+		if (response.status !== 201) throw new Error("Invalid Credentials!");
 
-        // Send HTTP Request 
-        const response = await axios.post(endPoint, data);
+		// Retrieve User Info
+		const token = response.data.token;
+		const userData = response.data.data.user;
 
-        // If Unauthorized, Stop and Throw Error
-        if (response.status !== 201) throw new Error("Invalid Credentials!");
+		// Save Token To Local Storage if the user wants to keep signed In
+		if (payload.keepSignedIn === true) {
+			localStorage.setItem("token", token);
+			localStorage.setItem("keepSignedIn", true);
+			localStorage.setItem("email", userData.email);
+			localStorage.setItem("name", userData.name);
+			localStorage.setItem("age", userData.age);
+			localStorage.setItem("country", userData.country);
+		}
 
-        // Retrieve User Info
-        const token = response.data.token;
-        const userData = response.data.data.user;
+		// Set User
+		context.commit("setUser", {
+			token: token,
+			email: userData.email,
+			name: userData.name,
+			age: userData.age,
+			country: userData.country,
+		});
 
-        // Save Token To Local Storage if the user wants to keep signed In
-        if (payload.keepSignedIn === true) {
-            localStorage.setItem('token', token);
-            localStorage.setItem('keepSignedIn', true);
-            localStorage.setItem('email', userData.email);
-            localStorage.setItem('name', userData.name);
-            localStorage.setItem('age', userData.age);
-            localStorage.setItem('country', userData.country);
-        }
+		await context.dispatch("getMainInfo");
 
-        // Set User
-        context.commit('setUser', {
-            token: token,
-            email: userData.email,
-            name: userData.name,
-            age: userData.age,
-            country: userData.country
-        });
+		timer = setInterval(async () => {
+			console.log("Calling Interval");
+			await context.dispatch("getMainInfo");
+		}, period);
+	},
 
-        await context.dispatch('getMainInfo');
+	// Sing In User Instan
+	autoSignIn(context) {
+		const token = localStorage.getItem("token");
+		const email = localStorage.getItem("email");
+		const name = localStorage.getItem("name");
+		const age = localStorage.getItem("age");
+		const country = localStorage.getItem("country");
+		const keepSignedIn = localStorage.getItem("keepSignedIn");
 
-        timer = setInterval(async () => {
-            console.log('Calling Interval');
-            await context.dispatch('getMainInfo');
-        }, period);
+		let userFound = false;
 
-    },
+		if (token && keepSignedIn === "true") {
+			// SET USER IN VUEX
+			context.commit("setUser", {
+				token,
+				email,
+				name,
+				age,
+				country,
+			});
 
-    // Sing In User Instan
-    autoSignIn(context) {
-        const token = localStorage.getItem('token');
-        const email = localStorage.getItem('email');
-        const name = localStorage.getItem('name');
-        const age = localStorage.getItem('age');
-        const country = localStorage.getItem('country');
-        const keepSignedIn = localStorage.getItem('keepSignedIn');
+			userFound = true;
 
-        let userFound = false;
+			timer = setInterval(async () => {
+				console.log("Calling Interval", period);
+				await context.dispatch("getMainInfo");
+			}, period);
+		}
 
-        if (token && keepSignedIn === "true") {
+		return userFound;
+	},
 
-            // SET USER IN VUEX
-            context.commit('setUser', {
-                token,
-                email,
-                name,
-                age,
-                country
-            });
+	// Sign Up Functionality
+	async signUp(context, payload) {
+		// API EndPoint
+		const endPoint = "https://cooperative-deer-overalls.cyclic.app/api/v1/users/signup";
 
-            userFound = true;
+		const data = {
+			name: payload.name,
+			email: payload.email,
+			password: payload.password,
+			passwordConfirm: payload.passwordConfirm,
+		};
 
-            timer = setInterval(async () => {
-                console.log('Calling Interval', period);
-                await context.dispatch('getMainInfo');
-            }, period);
-        }
+		// Send HTTP Request
+		const response = await axios.post(endPoint, data).catch(function (error) {
+			// console.log(error);
+			throw new Error(error.response.data.msg);
+		});
 
-        return userFound;
-    },
+		// Retrieve User Info
+		const token = response.data.token;
+		const userData = response.data.data.user;
 
-    // Sign Up Functionality
-    async signUp(context, payload) {
-        // API EndPoint
-        const endPoint = "https://cryptcize.herokuapp.com/api/v1/users/signup";
+		// Set User
+		context.commit("setUser", {
+			token: token,
+			email: userData.email,
+			name: userData.name,
+			age: userData.age,
+			country: userData.country,
+		});
 
-        const data = {
-            name: payload.name,
-            email: payload.email,
-            password: payload.password,
-            passwordConfirm: payload.passwordConfirm
-        };
+		await context.dispatch("getMainInfo");
 
-        // Send HTTP Request 
-        const response = await axios.post(endPoint, data).catch(function (error) {
-            // console.log(error);
-            throw new Error(error.response.data.msg);
-        });
+		timer = setInterval(async () => {
+			console.log("Calling Interval", period);
+			await context.dispatch("getMainInfo");
+		}, period);
+	},
 
-        // Retrieve User Info
-        const token = response.data.token;
-        const userData = response.data.data.user;
+	// Log User Out
+	logout(context) {
+		// Save Token To Local Storage
+		localStorage.removeItem("token");
+		localStorage.removeItem("keepSignedIn");
+		localStorage.removeItem("email");
+		localStorage.removeItem("name");
+		localStorage.removeItem("age");
+		localStorage.removeItem("country");
 
-        // Set User
-        context.commit('setUser', {
-            token: token,
-            email: userData.email,
-            name: userData.name,
-            age: userData.age,
-            country: userData.country
-        });
+		// Remove User Data from LocalStorage
+		context.commit("setUser", {
+			token: null,
+			email: null,
+			name: null,
+			age: null,
+			country: null,
+		});
 
-        await context.dispatch('getMainInfo');
+		clearInterval(timer);
+	},
 
-        timer = setInterval(async () => {
-            console.log('Calling Interval', period);
-            await context.dispatch('getMainInfo');
-        }, period);
-    },
+	// Update User's Personal Details
+	async updatePersonal(context, payload) {
+		// JWT Token
+		const token = context.rootGetters.token;
 
+		// API Endpoint To update user's personal details
+		const endPoint = "https://cooperative-deer-overalls.cyclic.app/api/v1/users/updatePersonal";
 
-    // Log User Out
-    logout(context) {
-        // Save Token To Local Storage
-        localStorage.removeItem('token');
-        localStorage.removeItem('keepSignedIn');
-        localStorage.removeItem('email');
-        localStorage.removeItem('name');
-        localStorage.removeItem('age');
-        localStorage.removeItem('country');
+		// New Data
+		const data = {
+			name: payload.name,
+			age: payload.age,
+			country: payload.country,
+		};
 
-        // Remove User Data from LocalStorage
-        context.commit('setUser', {
-            token: null,
-            email: null,
-            name: null,
-            age: null,
-            country: null
-        });
+		// Send HTTP Reques
+		const response = await axios
+			.patch(endPoint, data, { headers: { Authorization: `Bearer ${token}` } })
+			.catch(function (error) {
+				throw new Error(error.response.data.msg);
+			});
 
-        clearInterval(timer);
-    },
+		// Get if the user decided to be rememberd in the next visit
+		const keepSignedIn = localStorage.getItem("keepSignedIn");
 
+		// Only Assign personal Data is the user want to be logged in
+		if (keepSignedIn === "true") {
+			localStorage.setItem("name", response.data.data.user.name);
+			localStorage.setItem("age", response.data.data.user.age);
+			localStorage.setItem("country", response.data.data.user.country);
+		}
 
-    // Update User's Personal Details
-    async updatePersonal(context, payload) {
-        // JWT Token
-        const token = context.rootGetters.token;
+		// Update Application State Managment (Vuex)
+		context.commit("updateUserPersonal", {
+			name: response.data.data.user.name,
+			age: response.data.data.user.age,
+			country: response.data.data.user.country,
+		});
+	},
 
-        // API Endpoint To update user's personal details
-        const endPoint = "https://cryptcize.herokuapp.com/api/v1/users/updatePersonal";
+	// Update User's Security Details (password)
+	async updateSecurity(context, payload) {
+		// JWT Token
+		const token = context.rootGetters.token;
 
-        // New Data
-        const data = {
-            name: payload.name,
-            age: payload.age,
-            country: payload.country
-        };
+		// API Endpoint To update user's personal details
+		const endPoint = "https://cooperative-deer-overalls.cyclic.app/api/v1/users/updatePassword";
 
-        // Send HTTP Reques
-        const response = await axios.patch(endPoint, data, { headers: { "Authorization": `Bearer ${token}` } }).catch(function (error) {
-            throw new Error(error.response.data.msg);
-        });
+		// New Data
+		const data = {
+			currentPassword: payload.currentPassword,
+			password: payload.password,
+			passwordConfirm: payload.passwordConfirm,
+		};
 
-        // Get if the user decided to be rememberd in the next visit
-        const keepSignedIn = localStorage.getItem('keepSignedIn');
+		// Send HTTP Request
+		await axios.patch(endPoint, data, { headers: { Authorization: `Bearer ${token}` } }).catch(function (error) {
+			throw new Error(error.response.data.msg);
+		});
+	},
 
-        // Only Assign personal Data is the user want to be logged in
-        if (keepSignedIn === "true") {
-            localStorage.setItem('name', response.data.data.user.name);
-            localStorage.setItem('age', response.data.data.user.age);
-            localStorage.setItem('country', response.data.data.user.country);
-        }
+	// Send Email to user with reset password link
+	async forgotPassword(_, payload) {
+		// API Endpoint To update user's personal details
+		const endPoint = "https://cooperative-deer-overalls.cyclic.app/api/v1/users/forgotPassword";
 
-        // Update Application State Managment (Vuex)
-        context.commit('updateUserPersonal', {
-            name: response.data.data.user.name,
-            age: response.data.data.user.age,
-            country: response.data.data.user.country
-        });
-    },
+		// New Data
+		const data = {
+			email: payload.email,
+		};
 
+		// Send HTTP Reques
+		await axios.post(endPoint, data).catch(function (error) {
+			throw new Error(error.response.data.msg);
+		});
+	},
 
-    // Update User's Security Details (password)
-    async updateSecurity(context, payload) {
-        // JWT Token
-        const token = context.rootGetters.token;
+	// Reste User's Password Using Reset Link
+	async resetPassword(_, payload) {
+		console.log("RESETING PASSWORD ...");
 
-        // API Endpoint To update user's personal details
-        const endPoint = "https://cryptcize.herokuapp.com/api/v1/users/updatePassword";
+		// API Endpoint To reset user's password
+		const endPoint = `https://cooperative-deer-overalls.cyclic.app/api/v1/users/resetPassword/${payload.resetToken}`;
 
-        // New Data
-        const data = {
-            currentPassword: payload.currentPassword,
-            password: payload.password,
-            passwordConfirm: payload.passwordConfirm
-        };
+		// New Data
+		const data = {
+			password: payload.password,
+			passwordConfirm: payload.passwordConfirm,
+		};
 
-        // Send HTTP Request
-        await axios.patch(endPoint, data, { headers: { "Authorization": `Bearer ${token}` } }).catch(function (error) {
-            throw new Error(error.response.data.msg);
-        });
-    },
-
-
-    // Send Email to user with reset password link
-    async forgotPassword(_, payload) {
-
-        // API Endpoint To update user's personal details
-        const endPoint = "https://cryptcize.herokuapp.com/api/v1/users/forgotPassword";
-
-        // New Data
-        const data = {
-            email: payload.email
-        };
-
-        // Send HTTP Reques
-        await axios.post(endPoint, data).catch(function (error) {
-            throw new Error(error.response.data.msg);
-        });
-    },
-
-
-    // Reste User's Password Using Reset Link
-    async resetPassword(_, payload) {
-
-        console.log('RESETING PASSWORD ...');
-
-        // API Endpoint To reset user's password
-        const endPoint = `https://cryptcize.herokuapp.com/api/v1/users/resetPassword/${payload.resetToken}`;
-
-        // New Data
-        const data = {
-            password: payload.password,
-            passwordConfirm: payload.passwordConfirm
-        };
-
-        // Send HTTP Reques
-        await axios.patch(endPoint, data).catch(function (error) {
-            throw new Error(error.response.data.msg);
-        });
-    }
-
+		// Send HTTP Reques
+		await axios.patch(endPoint, data).catch(function (error) {
+			throw new Error(error.response.data.msg);
+		});
+	},
 };
